100d3a8fb98ccd924c58aaa74c9b29b246bf9b0a
Doug Heynderickx <doug_heynderickx@asl.org>
2014-10-31 12:02:45 +0000

    New runner class added
    For testing purposes?

---
+++ Primary Methods/src/primary/classes/PrimaryRunner.java
@@ -0,0 +1,16 @@
+package primary.classes;
+
+/**
+ * @author heynderickxd
+ * Main class to run the primary methods and interact with the user
+ * May serve for simple testing until GUI is developed
+ */
+public class PrimaryRunner {
+   public static void main(String[] args){
+       //Create new PrimaryMethods
+       PrimaryMethods pMeth = new PrimaryMethods();
+       System.out.println("Welcome to the Physics Calculator");
+       
+       //work terminated in favour of developing GIU. 
+   } 
+}
