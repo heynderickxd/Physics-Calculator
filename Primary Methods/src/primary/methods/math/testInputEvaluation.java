e9dd07908bfbc5847556752a25e3533ca0b13710
Doug Heynderickx <doug_heynderickx@asl.org>
2014-11-04 10:59:37 +0000

    Added test scanner class.

---
+++ Primary Methods/src/primary/methods/math/testInputEvaluation.java
@@ -0,0 +1,18 @@
+package primary.methods.math;
+/**
+ * @author heynderickxd
+ */
+import java.util.Scanner;
+public class testInputEvaluation {
+   public static void main(String[] args){
+       //use a scanner to test if a user input is equal or not to a double
+       Scanner input = new Scanner(System.in);
+       String userType = input.next("0.987");
+       if (input.hasNextDouble()){
+           System.out.println("You have entered a correct input!");
+       }
+       else {
+           System.out.println("You have not entered a correct input!");
+       }
+   } 
+}
